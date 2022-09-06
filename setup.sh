4816fedfb01af33439845cc0ca2c1cdd83aaf932
William Grim <code@williamgrim.com>
2022-09-06 17:40:01 +0100

    Add setup script to copy contents into the right locations.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_PATH="$(dirname "${0}")"
+pushd "${SCRIPT_PATH}" >/dev/null
+
+cat .gitconfig >> ~/.gitconfig
+cat .gitignore >> ~/.gitignore
+
+popd >/dev/null
